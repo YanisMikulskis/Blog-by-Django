631d0204469d7b195193ec71f9436a3369bd0811
Yanis37 <www.titan.ru95@mail.ru>
2024-02-11 02:46:59 +0300

    Создание порядка сортировки постов

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -7,6 +7,9 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        ordering = ['-publish']
     def __str__(self) -> str:
         return self.title
     
